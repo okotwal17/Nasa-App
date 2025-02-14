37448a582c2354439843f151cc7a900494dec6e3
okotwal17 <omkotwal1811@gmail.com>
2025-02-14 18:24:02 +0000

    Few styling errors fixed

--- nasa-react-app/src/App.jsx
+++ nasa-react-app/src/App.jsx
@@ -17,8 +17,6 @@ function App() {
       const NASA_KEY=import.meta.env.VITE_NASA_API_KEY
       const url="https://api.nasa.gov/planetary/apod" +  `?api_key=${NASA_KEY}`
 
-
-
       const today=(new Date()).toDateString()
       const localKey=`NASA-${today}`
       if(localStorage.getItem(localKey)) {
